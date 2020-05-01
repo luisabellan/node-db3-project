e46cca8aecac05b72aaae345ccd6258cf4ca99fc
Luis Abellan <luis.abellan.dev@gmail.com>
2020-05-01 06:56:08 +0200

    save

--- schemes/dist/scheme-model.dev.js
+++ schemes/dist/scheme-model.dev.js
@@ -1,5 +1,13 @@
 "use strict";
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var db = require('../data/config'); // resolves to a promise that resolves to an array of all schemes in the database.
 
 
@@ -27,14 +35,25 @@ function findSteps(id) {
   });
 }
 
-function remove(id) {
-  return db('schemes').where('scheme_id' === id).first().del();
+function remove(scheme) {
+  return db('schemes').where(scheme.id).first().del();
+}
+
+function add(scheme) {
+  var _db$insert = db('schemes').insert(scheme),
+      _db$insert2 = _slicedToArray(_db$insert, 1),
+      id = _db$insert2[0];
+
+  var sch = db('schemes').where({
+    id: id
+  }).first();
+  return sch;
 }
 
 module.exports = {
   find: find,
   findById: findById,
   findSteps: findSteps,
-  remove: remove,
-  add: add
+  add: add,
+  remove: remove
 };
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,51 +1,52 @@
-const db = require('../data/config')
+const db = require('../data/config');
 
 // resolves to a promise that resolves to an array of all schemes in the database.
 
- function  find(){
-    return  db('schemes')
+function find() {
+    return db('schemes')
 }
 
 // resolves to  Resolve to a single scheme object (or null)
 
-function findById(id){
-    return db('schemes').where({id}).first();
+function findById(id) {
+    return db('schemes').where({ id }).first();
 }
 
 // Resolves to an array of all correctly ordered step for the given scheme: `[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ]`.
-  function findSteps(id){
-   /*  SELECT s.id, k.scheme_name, s.step_number,s.instructions 
-    FROM steps AS s
-    JOIN [schemes] AS k
-    ON s.id = k.id; */
+function findSteps(id) {
+    /*  SELECT s.id, k.scheme_name, s.step_number,s.instructions 
+     FROM steps AS s
+     JOIN [schemes] AS k
+     ON s.id = k.id; */
 
-   // const steps =  db('steps').where({id}) 
-   
-   const  steps =  db('steps as s')
-   .join('schemes as k', 's.id', 'k.id')
-   .select('s.id', 'k.scheme_name', 's.step_number', 's.instructions')
-   
+    // const steps =  db('steps').where({id}) 
+
+    const steps = db('steps as s')
+        .join('schemes as k', 's.id', 'k.id')
+        .select('s.id', 'k.scheme_name', 's.step_number', 's.instructions');
 
 
-    return db(steps).where({id})
-} 
 
-funcion add(scheme) {
-    const [id] = await db("schemes").insert(req.body)
-    const schemes = await db("schemes").where({ id }).first()
-
+    return db(steps).where({ id });
 }
 
-function remove(id){
-    return db('schemes').where('scheme_id' === id).first().del()
+
+function remove(scheme) {
+    return db('schemes').where(scheme.id).first().del()
 
 }
 
 
+function add(scheme) {
+    const [id] =  db('schemes').insert(scheme)
+	const sch =  db('schemes').where({ id }).first()
+    return  sch
+
+}
 module.exports = {
     find,
     findById,
     findSteps,
+    add,
     remove,
-    add
 }
